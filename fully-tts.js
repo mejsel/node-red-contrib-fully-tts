38c576a1fde323f6983baacf003c732a7a9a55f3
Mejsel (Surface) <Mejsel (Surface)@localhost>
2018-08-19 21:22:19 +0200

    minimal functionality implemented, not tested

--- fully-tts.js
+++ fully-tts.js
@@ -3,19 +3,19 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 module.exports = function(RED) {
     function FullyTtsNode(config) {
         RED.nodes.createNode(this, config);
-        
-        this.on("input", (msg) => {
-            this.log("input message received");
-            const http = new XMLHttpRequest();
-            var node = this;
+        const http = new XMLHttpRequest();
+        var node = this;
+        node.log("instance created");
 
+        node.on("input", (msg) => {
+            node.log("input message received");
             http.onreadystatechange = function() {
                 if (http.readyState === 4) {
                     if (http.status === 200) {
                         node.log("success");
                         node.status({fill: "green", shape: "dot", text: "success"});
-                    //} else if (!isValid(this.response) && this.status == 0) {
-                    } else if (this.status == 0) {
+                    //} else if (!isValid(node.response) && this.status == 0) {
+                    } else if (node.status == 0) {
                         node.error("not responding");                
                         node.status({fill: "yellow", shape: "dot", text: "not responding"});
                     } else {
@@ -25,9 +25,8 @@ module.exports = function(RED) {
                 }
             };
 
-            const host = "192.168.0.173";
-            const password = "fullypw";
-            //const text = "Dies ist ein Test! A Ä u Ü o ö.";
+            var host = config.host;
+            var password = config.password;
             if (typeof msg.payload != "string")
             {
                 node.error("msg.payload has to be a string!");
@@ -36,7 +35,7 @@ module.exports = function(RED) {
             }
             var text = encodeURI(msg.payload);
             const requeststring = "http://"+host+":2323/?cmd=textToSpeech&text="+text+"&password="+password;
-            this.log(requeststring);
+            node.log(requeststring);
 
             http.open("GET", requeststring, true);
             http.send();
